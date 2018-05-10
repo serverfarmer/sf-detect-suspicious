1e44c219e4be466c773895555f2b210a5977a7ec
Tomasz Klim <github@tomaszklim.pl>
2018-05-10 22:27:18 +0200

    minor fix

--- cron/check.sh
+++ cron/check.sh
@@ -6,7 +6,7 @@ file=`mktemp`
 netstat -ap |grep ESTABLISHED |grep https |grep ec2 |grep -vFf /etc/local/.config/allowed.netstat >$file
 netstat -ap |grep ngrok |grep -vFf /etc/local/.config/allowed.netstat >>$file
 
-ps aux |egrep -i "(btc|carbon|coin|devfee|ether|miner|mining|torrent|xmr|yam)" |grep -v " grep " |grep -vFf /etc/local/.config/allowed.processes >>$file
+ps aux |egrep -i "(btc|carbon|coin|devfee|ether|miner|mining|torrent|xmr|yam)" |grep -v " egrep " |grep -vFf /etc/local/.config/allowed.processes >>$file
 
 if [ -s $file ]; then
 	cat $file |mail -s "Suspicious activity on host $HOST" unwanted-activities@`external_domain`
