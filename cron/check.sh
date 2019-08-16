ff61070a3ee353ec194d2491a3d9a4b1d7ed2c2d
Tomasz Klim <github@tomaszklim.pl>
2019-08-16 08:44:47 +0200

    mute suspicious alerts for connections without matched executable

--- cron/check.sh
+++ cron/check.sh
@@ -3,7 +3,7 @@
 . /opt/farm/scripts/functions.custom
 
 file=`mktemp`
-netstat -ap 2>/dev/null |grep ESTABLISHED |grep https |grep ec2 |egrep -v "(apache2|nginx)" |grep -vFf /etc/local/.config/allowed.netstat >$file
+netstat -ap 2>/dev/null |grep ESTABLISHED |grep -v "ESTABLISHED -" |grep https |grep ec2 |egrep -v "(apache2|nginx)" |grep -vFf /etc/local/.config/allowed.netstat >$file
 netstat -ap 2>/dev/null |grep ngrok |grep -vFf /etc/local/.config/allowed.netstat >>$file
 
 ps aux |egrep -i "(btc|carbon|coin|devfee|ether|miner|mining|torrent|xmr|yam)" |grep -v "grep " |grep -vFf /etc/local/.config/allowed.processes >>$file
