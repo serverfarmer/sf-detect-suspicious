d406f5f7e592bea7c776385723fefa6963b9ba3b
Tomasz Klim <github@tomaszklim.pl>
2020-02-03 08:51:43 +0100

    switch to new server farmer configuration layer

--- cron/check.sh
+++ cron/check.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 . /etc/farmconfig
-. /opt/farm/scripts/functions.custom
 
 file=`mktemp`
 netstat -ap 2>/dev/null |grep ESTABLISHED |grep -v "ESTABLISHED -" |grep https |grep ec2 |egrep -v "(apache2|nginx)" |grep -vFf /etc/local/.config/allowed.netstat >$file
@@ -9,7 +8,7 @@ netstat -ap 2>/dev/null |grep ngrok |grep -vFf /etc/local/.config/allowed.netsta
 ps aux |egrep -i "(btc|carbon|coin|devfee|ether|miner|mining|torrent|xmr|yam)" |grep -v "grep " |grep -vFf /etc/local/.config/allowed.processes >>$file
 
 if [ -s $file ]; then
-	cat $file |mail -s "Suspicious activity on host $HOST" unwanted-activities@`external_domain`
+	cat $file |mail -s "Suspicious activity on host $HOST" unwanted-activities@`/opt/farm/config/get-external-domain.sh`
 fi
 
 rm -f $file
