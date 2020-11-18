3781d1a602eb7e511b7771bbb449d7d3db915e9e
Tomasz Klim <github@tomaszklim.pl>
2020-11-18 09:54:44 +0100

    detect kinsing

--- cron/check.sh
+++ cron/check.sh
@@ -5,7 +5,7 @@ file=`mktemp`
 netstat -ap 2>/dev/null |grep ESTABLISHED |grep -v "ESTABLISHED -" |grep https |grep ec2 |egrep -v "(apache2|nginx)" |grep -vFf /etc/local/.config/allowed.netstat >$file
 netstat -ap 2>/dev/null |grep ngrok |grep -vFf /etc/local/.config/allowed.netstat >>$file
 
-ps aux |egrep -i "(btc|carbon|coin|devfee|ether|miner|mining|torrent|xmr|yam)" |grep -v "grep " |grep -vFf /etc/local/.config/allowed.processes >>$file
+ps aux |egrep -i "(btc|carbon|coin|devfee|ether|miner|mining|torrent|xmr|yam|kinsing|kdevtmpfsi|libsystem)" |grep -v "grep " |grep -vFf /etc/local/.config/allowed.processes >>$file
 
 if [ -s $file ]; then
 	cat $file |mail -s "Suspicious activity on host $HOST" unwanted-activities@`/opt/farm/config/get-external-domain.sh`
